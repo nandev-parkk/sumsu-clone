5993ce7b4935f4496d8df57bfb8cca9475ed8dd5
nandev <nandev.parkk@gmail.com>
2022-04-12 10:44:37 +0900

    footer 작업

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import Inner from "../layout/inner/Inner";
 import style from "./Footer.module.scss";
+import FooterNav from "./footerNav/FooterNav";
+import logo from "static/images/foot_logo.png";
 
 export default function Footer() {
   return (
     <footer>
-      <Inner>dd</Inner>
+      <FooterNav />
+      <Inner>
+        <div className={style.items}>
+          <a href="#">
+            <img src={logo} alt="로고" />
+          </a>
+
+          <div className={style.info}>
+            서울특별시 송파구 오금로 22길 13(송파동) (사)한국성서유니온선교회
+            사업자등록번호 215-82-10858 송파구 통신판매신고 제 4998호 <br />
+            대표 김주련 TEL : 02)2202-0091 FAX : 02)2202-0095
+            정기구독/쇼핑몰문의 02-6339-1277 E-mail : webmaster@su.or.kr
+            개인정보보호책임자 김종경 kim@su.or.kr <br />
+            ALL CONTENTS COPYRIGHT 2018 SCRIPTURE UNION KOREA
+          </div>
+        </div>
+      </Inner>
     </footer>
   );
 }
---
+++ src/components/common/footer/footerNav/FooterNav.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Inner from "components/common/layout/inner/Inner";
+import style from "./FooterNav.module.scss";
+
+export default function FooterNav() {
+  return (
+    <div className={style.container}>
+      <Inner>
+        <ul>
+          <li>
+            <a href="#">이용약관</a>
+          </li>
+          <li>
+            <a href="#" className={style.privacy}>
+              개인정보처리방침
+            </a>
+          </li>
+          <li>
+            <a href="#">이메일무단수집거부</a>
+          </li>
+          <li>
+            <a href="#">원격지원</a>
+          </li>
+        </ul>
+      </Inner>
+    </div>
+  );
+}
